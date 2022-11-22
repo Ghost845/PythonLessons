626aee75493794bd0f405437e36f1b8f69ac76ec
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-11-22 22:51:18 +0300

    first commit

---
+++ Lessons/lec.py
@@ -0,0 +1,43 @@
+# #value = None
+# #print(value)
+# #print(type(value))
+# a = 123
+# b = 12.34
+# #print(a)
+# #print(type(a))
+# #print(b)
+# #print(type(b))
+# #value = 12345
+# #print(value)
+# #print(type(value))
+
+# s = "hello 'world"
+# #s = 'hello "world'
+# #s = "hello \'world"
+# #s = "hello \nworld"
+# print(s)
+
+# print(a, b, s)
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'.format(a, b, s))
+# print('{1} - {0} - {2}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# Целочисленное
+print('Введите значение a: ')            
+a = int(input())
+print('Введите значение b: ')
+b = int(input())
+print(a, '+', b, '=', a+b)
+
+# Плавающее
+# print('Введите значение a: ')            
+# a = float(input())
+# print('Введите значение b: ')
+# b = float(input())
+# print(a, '+', b, '=', a+b)
+
+# / - деление с float значением, // - деление с выводом целого int числа
+# * - умножение, ** - возведение в степень
+
+с = round(a + b, 5)
